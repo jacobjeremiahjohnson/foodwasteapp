1e6e2ce61d10d9d4bfbf02aeb789442ba82152bd
Roger <57421158+RogerCronin@users.noreply.github.com>
2024-03-03 10:07:07 -0500

    ok

--- website/foodwasteapp/src/components/SupplierDashboard.js
+++ website/foodwasteapp/src/components/SupplierDashboard.js
@@ -3,8 +3,27 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { apiUrl } from "../App.js"
 import "./styles/SupplierDashboard.css"
-
 import picker from "../assets/picker.png"
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from "react-chartjs-2"
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 async function imageToImageUrl(file) {
     const body = new FormData()
@@ -46,7 +65,7 @@ export default function SupplierDashboard(props){
         .then(json => {
             setProducerInfo(json.data)
         })
-    }, [])
+    }, [navigate, props.token])
 
     useEffect(() => {
         if (Object.keys(orderInfo).length === 0){
@@ -66,7 +85,7 @@ export default function SupplierDashboard(props){
         .then(json => {
             console.log(json)
         })
-    }, [orderInfo])
+    }, [orderInfo, props.token])
 
     function onImageChange(e) {
         setImageUrl(URL.createObjectURL(e.target.files[0]))
@@ -94,22 +113,55 @@ export default function SupplierDashboard(props){
              <div className = "dashboardContainer">
                 <div className = "formContainer">
                     <form onSubmit={createOrder} className="dashboardForm">
-                        <div className="formTitle">
-                            Create listing
+                        <h2 className="formTitle">Create listing</h2>
+                        <div>
+                            <label for="supplyDescription">Describe your donation</label>
+                            <input id="supplyDescription" name="description" placeholder="Description"/>
                         </div>
-                        <input name="description" placeholder="Description of order"/>
-                        <input name="expiration" placeholder="Minutes until offer closes"/>
-                        <div className="weightLabel">
-                            How much food is able to be donated?
+                        <div>
+                            <label for="supplyExpiration">How many minutes until this listing expires?</label>
+                            <input id="supplyExpiration" name="expiration" placeholder="Minutes"/>
                         </div>
-                        <input name="weight" placeholder="How much food is able to be donated (lbs.)"/>
-                        <input name="image-url" type="file" onChange={onImageChange}></input>
-                        <img className="uploadedImg" src={imageUrl} alt="Your custom upload"/>
+                        <div>
+                            <label for="supplyWeight">How many pounds of food is this donation?</label>
+                            <input id="supplyWeight" name="weight" placeholder="Weight"/>
+                        </div>
+                        <div id="uploadImageDiv">
+                            <label for="supplyImage">Upload an image of your donation</label>
+                            <img className="uploadedImg" src={imageUrl} alt="Your custom upload"/>
+                            <input id="supplyImage" name="image-url" type="file" onChange={onImageChange}/>
+                        </div>
                         <button type="submit">Upload!</button>
                     </form>
                 </div>
                 <div className = "widgetContainer">
-
+                    <h2>You've donated <b>85</b> pounds of food this week</h2>
+                    <Bar
+                        className="barChart"
+                        data={{
+                            labels: ["2/26", "2/27", "2/28", "2/29", "3/1", "3/2", "3/3"],
+                            datasets: [
+                                {
+                                    label: "Pounds",
+                                    data: [8, 7, 7, 8, 3, 6, 6],
+                                    backgroundColor: "blue",
+                                }
+                            ]
+                        }}
+                        options={{
+                            maintainAspectRatio: true,
+                            responsive: true,
+                            plugins: {
+                                legend: {
+                                    display: false
+                                },
+                                title: {
+                                    display: true,
+                                    text: "Pounds of food donated"
+                                }
+                            }
+                        }}
+                    />
                 </div>
             </div>
         </div>
