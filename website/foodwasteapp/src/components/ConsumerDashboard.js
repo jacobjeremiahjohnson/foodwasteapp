d0890abb92dab864554345d206378d943cf41f6c
Roger <57421158+RogerCronin@users.noreply.github.com>
2024-03-03 10:14:58 -0500

    separate card

--- website/foodwasteapp/src/components/ConsumerDashboard.js
+++ website/foodwasteapp/src/components/ConsumerDashboard.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { apiUrl } from "../App.js"
+import { OrderCard } from "./OrderCard.js"
 import "./styles/Dashboard.css"
 
 export default function ConsumerDashboard(props){
@@ -10,23 +11,6 @@ export default function ConsumerDashboard(props){
     const [orderObject, setOrders] = useState({})
     const [displayOrder, setDisplayOrder] = useState(false)
     const [orderDetails, setOrderDetails] = useState({})
-
-    // async function imageToImageUrl(file) {
-    //     const body = new FormData()
-    //     body.append("image", file)
-    //     console.log(body)
-    //     try {
-    //         const res = await fetch(`https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_img_bb_api_key}`, {
-    //             method: "POST",
-    //             body: body
-    //         })
-    //         const json = await res.json()
-    //         return json.data.image.url
-    //     } catch(ex) {
-    //         console.error(ex)
-    //         alert("error uploading image")
-    //     }
-    // }
 
     useEffect(() => {
         if (!props.token){
@@ -60,7 +44,7 @@ export default function ConsumerDashboard(props){
         .then(json => {
             setConsumerInfo(json.data)
         })
-    }, [])
+    }, [navigate, props.token])
 
     useEffect(() => {
         if(Object.keys(orderObject) === 0){
@@ -78,65 +62,6 @@ export default function ConsumerDashboard(props){
         
     }
 
-    function OrderCard(props){
-        console.log(props)
-
-        return(
-            <div>
-            <div className="orderContainer">
-                <div className="imageContainer">
-                    <img className="image" src={props.data.image_url} />
-                </div>
-                <div className="orderTextContainer">
-                    <div className="restaurantTitle">
-                        {props.data.name}
-                    </div>
-                    <div className="address">
-                        {props.data.location.address}
-                    </div>
-                    <div className="description">
-                        {props.data.description}
-                    </div>
-                </div>
-                {props.data.status === "expired" && 
-                    <div className="expirationNotifier" style = {{ backgroundColor: "#BD0008" }}>
-                        <div className="emojiContainer">
-                            🗑️
-                        </div>
-                        <div className="expiration">
-                            {props.data.status}
-                        </div>
-                    </div>
-                    }
-                    {
-                        props.data.status === "closed" &&
-                        <div className="expirationNotifier" style = {{ backgroundColor: "grey" }}>
-                            <div className="emojiContainer">
-                            ✔️
-                            </div>
-                                <div className="expiration">
-                            {props.data.status}
-                            </div>
-                        </div>
-                    }
-                    {
-                        props.data.status === "open" &&
-                        <button className="expirationNotifier" style = {{ backgroundColor: "#90C418" }} onClick={(e) => {
-                            handleClick(e, props.data)
-                        }}>
-                            <div className="emojiContainer">
-                            🕒
-                            </div>
-                            <div className="expiration" >
-                            {props.data.status}
-                            </div>
-                        </button>
-                    }
-                </div>
-            </div>
-        )
-    }
-
     function OrderCardList(){
         if (displayOrder){
             console.log(orderObject.orders)
@@ -146,7 +71,7 @@ export default function ConsumerDashboard(props){
                     {arr.map(function(order){
                         return (
                         <li key={order.id}>
-                            <OrderCard data={order}/>
+                            <OrderCard data={order} handleClick={handleClick}/>
                         </li>
                         )
                     }
