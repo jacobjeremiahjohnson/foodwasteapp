c3d7b0427a2885af99ae0e674ef1f88ca1238b97
Jacob Johnson <jacobjeremiah42@gmail.com>
2024-03-03 08:41:32 -0500

    day 2 start

--- website/foodwasteapp/src/components/ConsumerDashboard.js
+++ website/foodwasteapp/src/components/ConsumerDashboard.js
@@ -7,13 +7,13 @@ import "./styles/Dashboard.css"
 export default function ConsumerDashboard(props){
     const navigate = useNavigate()
     const [consumerInfo, setConsumerInfo] = useState({})
-    const [orders, setOrders] = useState({})
+    const [orderObject, setOrders] = useState({})
+    const [displayOrder, setDisplayOrder] = useState(false)
 
     useEffect(() => {
         if (!props.token){
             navigate("/login")
         }
-        console.log(props.token)
         fetch(apiUrl + "order/nearby-orders", {
             method: 'GET',
             mode: 'cors',
@@ -26,7 +26,7 @@ export default function ConsumerDashboard(props){
         .then(response => response.json())
         .then(json => {
             console.log(json.data)
-            setOrders(json.data.orders)
+            setOrders(json.data)
         })
 
         fetch(apiUrl + "account/me", {
@@ -44,26 +44,72 @@ export default function ConsumerDashboard(props){
         })
     }, [])
 
-    function orderCard(){
-        const orderItems = orders.orders.map((order) => 
-            <li key={order.id}>
-                <div className = "imageContainer">
-                    Image
-                </div>
-                <div className = "textContent">
-                    {order.description}
-                </div>
-                <div className = "claimButton">
-                    button
-                </div>
-            </li>
-        )
+    useEffect(() => {
+        if(Object.keys(orderObject) === 0){
+            return
+        }
+        setDisplayOrder(true)
+        console.log(orderObject)
+    }, [orderObject])
+
+    function OrderCard(props){
+        console.log(props)
+
         return(
-        <div className = "orderCard">
-            <ul>
-                {orderItems}
-            </ul>
-        </div>
+            <div>
+            <div className="orderContainer">
+                <div className="imageContainer">
+                    {props.data.image_url}
+                </div>
+                <div className="orderTextContainer">
+                    <div className="restaurantTitle">
+                        {props.data.name}
+                    </div>
+                    <div className="address">
+                        {props.data.address}
+                    </div>
+                    <div className="description">
+                        {props.data.description}
+                    </div>
+                </div>
+                <div className="expirationNotifier">
+                    {props.data.status}
+                </div>
+            </div>
+            </div>
+        )
+    }
+
+    function OrderCardList(){
+        if (displayOrder){
+            console.log(orderObject.orders)
+            const arr = orderObject.orders
+            return(
+                <ul>
+                    {arr.map(function(order){
+                        return (
+                        <li key={order.id}>
+                            <OrderCard data={order}/>
+                        </li>
+                        )
+                    }
+                    )}
+                </ul>
+            )
+        } else {
+            console.log("not displaying orders")
+            return(
+            <div>
+                
+            </div>)
+        }
+    }
+
+    if(Object.keys(orderObject).length === 0) {
+        return (
+            <div>
+                No data
+            </div>
         )
     }
 
@@ -71,7 +117,7 @@ export default function ConsumerDashboard(props){
         <div className = "main">
             <div className = "title">{consumerInfo.name + " Dashboard"}</div>
             <div className = "orderList">
-                {orderCard}
+                <OrderCardList />
             </div>
         </div>
     )
